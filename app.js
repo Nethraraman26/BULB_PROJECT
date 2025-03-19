8b9085cb8ae60905b24a0c15b2abd8bd6b16c58d
Nethraraman26 <nethraraman2006@gmail.com>
2025-03-19 07:25:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_button=document.getElementById("on");
+var off_button=document.getElementById("off");
+var img=document.getElementById("bulb");
+function bulb_on()
+{
+    img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDbzKMyDNLLcL2w17gQH1f60nLCLGrUt5Wcg&s";
+}
+function bulb_off()
+{
+    img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSloH-FFMUYIlsSSRMKZX_5ZMSTRC9C0T74QA&s";
+}
